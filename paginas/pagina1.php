0d54a3043b46cfab5e7a01c1babaee0212da1307
iLevsPHP_ <202215210015@ifba.edu.br>
2023-05-16 11:20:42 -0300

    Adicionando pastas novas e organizando links

--- paginas/pagina1.php
+++ paginas/pagina1.php
@@ -8,7 +8,7 @@
     <title>Consulte</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="estilos/estilo.css" rel="stylesheet">
+    <link href="../estilos/estilo.css" rel="stylesheet">
 </head>
 <body>
     <header>
