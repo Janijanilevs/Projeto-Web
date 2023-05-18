f4df6308ca01e93acb277599ef75fb75e1fbb4e5
iLevsPHP_ <202215210015@ifba.edu.br>
2023-05-18 20:15:15 -0300

    Pequeno realocamento de paginas

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
         <title>Consulta+ | Homepage</title>
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link href="../estilos/estilo.css" rel="stylesheet">
+        <link href="estilos/estilo.css" rel="stylesheet">
     </head>
     <body>
         <header id="topo">
@@ -30,10 +30,10 @@
                     <h2 class="titulo">O QUE VOCÊ GOSTARIA DE AGENDAR?</h2>
                     <h3 class="sub">AGENDE SUAS CONSULTAS E TELECONSULTAS RÁPIDO E FÁCIL.</h3>
                     <form action="" method="get">
-                        <button><img src="../img/busca.png"></button>
+                        <button><img src="img/busca.png"></button>
                         <input type="search" name="agendamento" placeholder="  O que gostaria de agendar?">
                     </form>
-                    <img src="../img/medicos.png" alt="médicos do Consulta+">
+                    <img src="img/medicos.png" alt="médicos do Consulta+">
             </section>
         
             <section id="detalhes-empresa">
@@ -41,15 +41,15 @@
                 <div class="container-itens">
                     <article class="item">
                         <h3>Agenda Online Flexível</h3>
-                        <img src="../img/check.png" alt="check">
+                        <img src="img/check.png" alt="check">
                     </article>
                     <article class="item">
                         <h3>Especialidades Médicas Diversas</h3>               
-                        <img src="../img/icon.png" alt="doutor">
+                        <img src="img/icon.png" alt="doutor">
                     </article>
                     <article class="item">
                         <h3>Fácil Implementação</h3>
-                        <img src="../img/check.png" alt="check">
+                        <img src="img/check.png" alt="check">
                     </article>
                 </div>
                 <div class="botao-saibamais">
