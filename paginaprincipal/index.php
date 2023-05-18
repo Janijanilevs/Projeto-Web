c28e55308f6fed9e02d22263461120f1acc3f9de
Joana Caires <joanacrsscbral@gmail.com>
2023-05-18 00:19:06 -0300

    alterando site, adicionando fonte nunito

--- paginaprincipal/index.php
+++ paginaprincipal/index.php
@@ -5,7 +5,7 @@
 <html lang="pt-br">
 <head>
     <meta charset="UTF-8">
-    <title>Consulte</title>
+    <title>Consulta+ | Homepage</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="../estilos/estilo.css" rel="stylesheet">
@@ -30,8 +30,8 @@
             <h2 class="titulo">O QUE VOCÊ GOSTARIA DE AGENDAR?</h2>
             <h3 class="sub">AGENDE SUAS CONSULTAS E TELECONSULTAS RÁPIDO E FÁCIL.</h3>
             <form action="" method="get">
-                <input type="search" name="agendamento" placeholder="O que gostaria de agendar?">
                 <button><img src="../img/busca.png"></button>
+                <input type="search" name="agendamento" placeholder="  O que gostaria de agendar?">
             </form>
             <img src="../img/medicos.png" alt="médicos do Consulta+">
         </section>
