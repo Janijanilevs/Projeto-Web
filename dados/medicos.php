d681593c97cfc62e136fc0fc05383057e13cda0f
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-05-18 22:51:36 -0300

    update medicos 2

--- dados/medicos.php
+++ dados/medicos.php
@@ -1,6 +1,8 @@
 <?php
 /*4 Cirurgião plástico, 5 Odontologia, 5 Nutricionista, 5 Genicologista, 1 Cardiologista*/
 $medicos =[
+    /*-----------------------Parte dos Cardiologistas---------------------------------------------------------------------------------*/
+
     [
         "nome" => "Maria Eduarda",
         "especialidade" => "Cardiologista",
@@ -12,35 +14,60 @@ $medicos =[
         "nome" => "Fábio de Oliveira",
         "especialidade" => "Cardiologista",
         "foto" => "https://i.pinimg.com/564x/e2/56/f2/e256f28fb227248309d94f68a629bcfc.jpg",
-        "formação" => "Formado em medicina pela UFBA, com mais de 15 anos trabalhando na área, ele é especializado em cardiologia infantil, checando os batimentos do seu pequeno"   
+        "formação" => "Formado em medicina pela UFBA, com mais de 15 anos trabalhando na área, ele é especializado em cardiologia infantil, checando os batimentos do seu pequeno."   
     ],
 
     [
         "nome" => "João Valadares",
         "especialidade" => "Cardiologista",
         "foto" => "https://i.pinimg.com/564x/95/28/67/952867678246dd51b3fa7663bec27aea.jpg",
-        "formação" => "Formado em medicina pela UFMG, com mais de 20 anos trabalhando na área, e sendo sua especialidade um tratamento intensivo, voltado para fazer a sua cirurgia "   
+        "formação" => "Formado em medicina pela UFMG, com mais de 20 anos trabalhando na área, e sendo sua especialidade um tratamento intensivo, voltado para fazer a sua cirurgia. "   
     ],
     [
         "nome" => "Ana Maria",
         "especialidade" => "Cardiologista",
         "foto" => "https://i.pinimg.com/564x/a4/0b/b5/a40bb56caa09bc24c3ece9662ebc9593.jpg",
-        "formação" => "Formada em medicina pela USP, com mais de 8 anos trabalhando na área, ela vai te ajudar a se recuperar de lesões, especializada no ramo da Ergometria e reabilitação, propõe com sua didática um melhor condicionamento do coração"   
+        "formação" => "Formada em medicina pela USP, com mais de 8 anos trabalhando na área, ela vai te ajudar a se recuperar de lesões, especializada no ramo da Ergometria e reabilitação, propõe com sua didática um melhor condicionamento do coração."   
     ],
 
     [
         "nome" => "Leviatan Ubirajara",
         "especialidade" => "Cardiologista",
         "foto" => "https://i.pinimg.com/564x/7d/6c/37/7d6c37451113490accea2dca24dc442d.jpg",
-        "formação" => "Formado em medicina pela USP, com mais de 50 anos trabalhando na área, um renomado médico no setor intensivista, focado no estudo sobre arritimias Eletrofisiologia Cardíaca"   
+        "formação" => "Formado em medicina pela USP, com mais de 50 anos trabalhando na área, um renomado médico no setor intensivista, focado no estudo sobre arritimias Eletrofisiologia Cardíaca."   
     ],
-
+/*-----------------------Parte dos Cirurgiões Plásticos-------------------------------------------------------------------------------*/
     [
         "nome" => "Milton Ricardo",
         "especialidade" => "Cirurgião Plástico",
         "foto" => "https://i.pinimg.com/originals/3d/c8/f2/3dc8f2c8fa0a30baef024dcf00cc3f37.jpg",
-        "formação" => "Formado em medicina pela UNIFG,especializado em implantes de silicone, mamoplastia, abdominoplastia e lipo HD, com mais de 10 anos transformando vidas.",
+        "formação" => "Formado em medicina pela UNIFG,especializado em implantes de silicone, mamoplastia, abdominoplastia e lipo HD, com mais de 10 anos transformando vidas."
         
       ],
-  
+
+      [
+        "nome" => "Geovana Aparecida",
+        "especialidade" => "Cirurgiã Plástico",
+        "foto" => "https://i.pinimg.com/originals/3d/c8/f2/3dc8f2c8fa0a30baef024dcf00cc3f37.jpg",
+        "formação" => "Formada em medicina pela UNIFESP,com especialidade em questões ligadas ao câncer de pele, sendo elas: retirada de tumores, remoção de ccatrizes, ou até mesmo implantação de enxertos"
+      ],
+
+      [
+        "nome" => "Marcos Albuquerque",
+        "especialidade" => "Cirurgião Plástico",
+        "foto" => "https://i.pinimg.com/564x/2e/59/80/2e5980b48418fa8ce49c64bf6e66186f.jpg",
+        "formação" => "Formado em medicina pela UNICAMP,com formação para cuidados com queimaduras, de 1º, 2º, 3º grau, para minimizar os efeitos em sua pele."
+      ],
+      [
+        "nome" => "Eloah Luz",
+        "especialidade" => "Cirurgiã Plástico",
+        "foto" => "https://i.pinimg.com/564x/e9/a6/01/e9a601c37cc6f0f1e08e8c9204ac7e41.jpg",
+        "formação" => "Formada em medicina pela UNB, e com especialidade em cosmiatria, cuidando para retardar o envelhecimento de sua pele"
+      ],
+      [
+        "nome" => "Murilo Bittencourt",
+        "especialidade" => "Cirurgião Plástico",
+        "foto" => "https://i.pinimg.com/564x/3b/09/84/3b098458469a2e6b805da7f078f45ba6.jpg",
+        "formação" => "Formada em medicina pela UFF, e com especialidade em plástica pediatra, cuidando de situações de má formações e situações com crianças"
+      ],
 ];
