16166ef4dc4cf11ca85af38e38e12e5e8b1ebcd3
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-05-18 23:21:01 -0300

    update medicos 3

--- dados/medicos.php
+++ dados/medicos.php
@@ -1,5 +1,5 @@
 <?php
-/*4 Cirurgião plástico, 5 Odontologia, 5 Nutricionista, 5 Genicologista, 1 Cardiologista*/
+/* 5 Odontologia, 5 Genicologista,*/
 $medicos =[
     /*-----------------------Parte dos Cardiologistas---------------------------------------------------------------------------------*/
 
@@ -37,7 +37,7 @@ $medicos =[
         "formação" => "Formado em medicina pela USP, com mais de 50 anos trabalhando na área, um renomado médico no setor intensivista, focado no estudo sobre arritimias Eletrofisiologia Cardíaca."   
     ],
 /*-----------------------Parte dos Cirurgiões Plásticos-------------------------------------------------------------------------------*/
-    [
+      [
         "nome" => "Milton Ricardo",
         "especialidade" => "Cirurgião Plástico",
         "foto" => "https://i.pinimg.com/originals/3d/c8/f2/3dc8f2c8fa0a30baef024dcf00cc3f37.jpg",
@@ -58,16 +58,55 @@ $medicos =[
         "foto" => "https://i.pinimg.com/564x/2e/59/80/2e5980b48418fa8ce49c64bf6e66186f.jpg",
         "formação" => "Formado em medicina pela UNICAMP,com formação para cuidados com queimaduras, de 1º, 2º, 3º grau, para minimizar os efeitos em sua pele."
       ],
+
       [
         "nome" => "Eloah Luz",
         "especialidade" => "Cirurgiã Plástico",
         "foto" => "https://i.pinimg.com/564x/e9/a6/01/e9a601c37cc6f0f1e08e8c9204ac7e41.jpg",
-        "formação" => "Formada em medicina pela UNB, e com especialidade em cosmiatria, cuidando para retardar o envelhecimento de sua pele"
+        "formação" => "Formada em medicina pela UNB, e com especialidade em cosmiatria, cuidando para retardar o envelhecimento de sua pele."
       ],
+
       [
         "nome" => "Murilo Bittencourt",
         "especialidade" => "Cirurgião Plástico",
         "foto" => "https://i.pinimg.com/564x/3b/09/84/3b098458469a2e6b805da7f078f45ba6.jpg",
-        "formação" => "Formada em medicina pela UFF, e com especialidade em plástica pediatra, cuidando de situações de má formações e situações com crianças"
+        "formação" => "Formada em medicina pela UFF, e com especialidade em plástica pediatra, cuidando de situações de má formações e situações com crianças."
       ],
-];
+      /*-----------------------Parte dos Nutricionistas-------------------------------------------------------------------------------*/
+
+      [
+        "nome" => "Layana Vasconcelos",
+        "especialidade" => " Nutricionista",
+        "foto" => "https://i.pinimg.com/564x/72/54/4c/72544c485dbb0f4c4004311bcc7e9450.jpg",
+        "formação" => "Formada em nutrição pela UFRB, com sua especialidade nutrição clínica, com receituários para uma boa dieta."
+      ],
+
+      [
+        "nome" => "Adalberto Moreira",
+        "especialidade" => " Nutricionista",
+        "foto" => "https://img.freepik.com/fotos-gratis/retrato-do-doutor-bonito-com-casaco-azul-olhando-para-a-camera_171337-15911.jpg?w=1060&t=st=1684462215~exp=1684462815~hmac=6f96ebb739d004f72245ad97460737b20d03302fd2018799887b12eae6d4034f",
+        "formação" => "Formada em nutrição pela UFRN, com sua especialidade nutrição do esporte, conciliando uma rotina intensa de 1 atleta com uma alimentação balanceada."
+      ],
+
+      [
+        "nome" => "Jonathan Ribeiro",
+        "especialidade" => " Nutricionista",
+        "foto" => "https://i.pinimg.com/736x/4b/ea/99/4bea99eb80bbd65cce12c9bc66717d02.jpg",
+        "formação" => "Formada em nutrição pela UFOP, com sua especialidade nutrição familiar, fazendo com que a família se adeque a um novo estilo de alimentar, mais saudável, causando menos traumas"
+      ],
+
+      [
+        "nome" => "Manuela Santos",
+        "especialidade" => " Nutricionista",
+        "foto" => "https://i.pinimg.com/564x/6d/3b/a9/6d3ba905d73028c0f98e0a419cf5ecaa.jpg",
+        "formação" => "Formada em nutrição pela UFPB, com sua especialidade em segurança dos alimentos, proporcionando uma maior qualidade de consumo de alimentos."
+      ],
+
+      [
+        "nome" => "Ingrind Silva",
+        "especialidade" => " Nutricionista",
+        "foto" => "https://i.pinimg.com/736x/4a/ca/c4/4acac4216f3d036467cc8cd1f720c993.jpg",
+        "formação" => "Formada em nutrição pela IFRS, com sua especialidade Nutrição ortomolecular, trazendo análises terapêuticas das deficiências de nutrientes e das características genéticas do paciente"
+      ],
+
+    ];
