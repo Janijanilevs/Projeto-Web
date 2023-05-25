f1fa0c60e1d379af4b03767d1daf96931016a978
Luan Caires <113989560+luan-caires@users.noreply.github.com>
2023-05-25 10:51:50 -0300

    att 25/04

--- dados/medicos.php
+++ dados/medicos.php
@@ -14,34 +14,34 @@ $medicos =[
     [
         "nome" => "Fábio de Oliveira",
         "especialidade" => "Cardiologia",
-        "foto" => "https://i.pinimg.com/564x/e2/56/f2/e256f28fb227248309d94f68a629bcfc.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/fabio_card.jpg",
         "formação" => "Formado em medicina pela UFBA, com mais de 15 anos trabalhando na área, ele é especializado em cardiologia infantil, checando os batimentos do seu pequeno."   
     ],
 
     [
         "nome" => "João Valadares",
         "especialidade" => "Cardiologia",
-        "foto" => "https://i.pinimg.com/564x/95/28/67/952867678246dd51b3fa7663bec27aea.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/joao_card.jpg",
         "formação" => "Formado em medicina pela UFMG, com mais de 20 anos trabalhando na área, e sendo sua especialidade um tratamento intensivo, voltado para fazer a sua cirurgia. "   
     ],
     [
         "nome" => "Ana Maria",
         "especialidade" => "Cardiologia",
-        "foto" => "https://i.pinimg.com/564x/a4/0b/b5/a40bb56caa09bc24c3ece9662ebc9593.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/anamaria_card.jpg",
         "formação" => "Formada em medicina pela USP, com mais de 8 anos trabalhando na área, ela vai te ajudar a se recuperar de lesões, especializada no ramo da Ergometria e reabilitação, propõe com sua didática um melhor condicionamento do coração."   
     ],
 
     [
         "nome" => "Leviatan Ubirajara",
         "especialidade" => "Cardiologia",
-        "foto" => "https://i.pinimg.com/564x/7d/6c/37/7d6c37451113490accea2dca24dc442d.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/leviatan_card.jpg",
         "formação" => "Formado em medicina pela USP, com mais de 50 anos trabalhando na área, um renomado médico no setor intensivista, focado no estudo sobre arritimias Eletrofisiologia Cardíaca."   
     ],
 /*-----------------------Parte dos Cirurgiões Plásticos-------------------------------------------------------------------------------*/
       [
         "nome" => "Milton Ricardo",
         "especialidade" => "Cirurgia Plástica",
-        "foto" => "https://i.pinimg.com/originals/3d/c8/f2/3dc8f2c8fa0a30baef024dcf00cc3f37.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/ric_card.jpg",
         "formação" => "Formado em medicina pela UNIFG,especializado em implantes de silicone, mamoplastia, abdominoplastia e lipo HD, com mais de 10 anos transformando vidas."
         
       ],
@@ -49,99 +49,98 @@ $medicos =[
       [
         "nome" => "Geovana Aparecida",
         "especialidade" => "Cirurgia Plástica",
-        "foto" => "https://i.pinimg.com/236x/c3/1f/78/c31f78b170e7a95af053d4e745f62a47.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/geovana_card.jpg",
         "formação" => "Formada em medicina pela UNIFESP,com especialidade em questões ligadas ao câncer de pele, sendo elas: retirada de tumores, remoção de ccatrizes, ou até mesmo implantação de enxertos"
       ],
 
       [
         "nome" => "Marcos Albuquerque",
         "especialidade" => "Cirurgia Plástica",
-        "foto" => "https://i.pinimg.com/564x/2e/59/80/2e5980b48418fa8ce49c64bf6e66186f.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/marco_card.jpg",
         "formação" => "Formado em medicina pela UNICAMP,com formação para cuidados com queimaduras, de 1º, 2º, 3º grau, para minimizar os efeitos em sua pele."
       ],
 
       [
         "nome" => "Eloah Luz",
         "especialidade" => "Cirurgia Plástica",
-        "foto" => "https://i.pinimg.com/564x/e9/a6/01/e9a601c37cc6f0f1e08e8c9204ac7e41.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/eloah_card.jpg",
         "formação" => "Formada em medicina pela UNB, e com especialidade em cosmiatria, cuidando para retardar o envelhecimento de sua pele."
       ],
 
       [
         "nome" => "Murilo Bittencourt",
         "especialidade" => "Cirurgia Plástica",
-        "foto" => "https://i.pinimg.com/564x/3b/09/84/3b098458469a2e6b805da7f078f45ba6.jpg",
-        "formação" => "Formada em medicina pela UFF, e com especialidade em plástica pediatra, cuidando de situações de má formações e situações com crianças."
+        "foto" => "../../Projeto-Web/img/medicos/murilo_card.jpg",
+        "formação" => "Formado em medicina pela UFF, e com especialidade em plástica pediatra, cuidando de situações de má formações e situações com crianças."
       ],
       /*-----------------------Parte dos Nutricionistas-------------------------------------------------------------------------------*/
 
       [
         "nome" => "Layana Vasconcelos",
         "especialidade" => "Nutrição",
-        "foto" => "https://i.pinimg.com/564x/21/16/8a/21168a07c442ee1c223ce9c4a7cbdaf7.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/layana_card.jpg",
         "formação" => "Formada em nutrição pela UFRB, com sua especialidade nutrição clínica, com receituários para uma boa dieta."
       ],
 
       [
         "nome" => "Adalberto Moreira",
         "especialidade" => "Nutrição",
-        "foto" => "https://img.freepik.com/fotos-gratis/retrato-do-doutor-bonito-com-casaco-azul-olhando-para-a-camera_171337-15911.jpg?w=1060&t=st=1684462215~exp=1684462815~hmac=6f96ebb739d004f72245ad97460737b20d03302fd2018799887b12eae6d4034f",
-        "formação" => "Formada em nutrição pela UFRN, com sua especialidade nutrição do esporte, conciliando uma rotina intensa de 1 atleta com uma alimentação balanceada."
+        "foto" => "../../Projeto-Web/img/medicos/adalberto_card.jpg", "formação" => "Formada em nutrição pela UFRN, com sua especialidade nutrição do esporte, conciliando uma rotina intensa de 1 atleta com uma alimentação balanceada."
       ],
 
       [
         "nome" => "Jonathan Ribeiro",
         "especialidade" => "Nutrição",
-        "foto" => "https://i.pinimg.com/736x/4b/ea/99/4bea99eb80bbd65cce12c9bc66717d02.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/adalberto_card.jpg",
         "formação" => "Formada em nutrição pela UFOP, com sua especialidade nutrição familiar, fazendo com que a família se adeque a um novo estilo de alimentar, mais saudável, causando menos traumas"
       ],
 
       [
         "nome" => "Manuela Santos",
         "especialidade" => "Nutrição",
-        "foto" => "https://i.pinimg.com/564x/6d/3b/a9/6d3ba905d73028c0f98e0a419cf5ecaa.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/manuela_card.jpg",
         "formação" => "Formada em nutrição pela UFPB, com sua especialidade em segurança dos alimentos, proporcionando uma maior qualidade de consumo de alimentos."
       ],
 
       [
         "nome" => "Ingrind Silva",
         "especialidade" => "Nutrição",
-        "foto" => "https://i.pinimg.com/736x/4a/ca/c4/4acac4216f3d036467cc8cd1f720c993.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/ingrid_card.jpg",
         "formação" => "Formada em nutrição pela IFRS, com sua especialidade Nutrição ortomolecular, trazendo análises terapêuticas das deficiências de nutrientes e das características genéticas do paciente"
       ],
 /*----------------------------Parte dos Ginecologistas-------------------------------------------------------------------------------*/
       [
         "nome" => "Luiz Miguel",
         "especialidade" => "Ginecologia",
-        "foto" => "https://i.pinimg.com/564x/4a/b9/3e/4ab93ecfc2c66ed78eda01b78aa17ded.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/luiz_card.jpg",
         "formação" => "Formado em medicina pela UFPEL, com sua especialidade Ginecologia endócrina, tratando de questões hormonais e reprodutivos, desde o intra-útero até a sensibilidade, com muita confiaça e descrição"
       ],
 
       [
         "nome" => "Mariane Melo",
         "especialidade" => "Ginecologia",
-        "foto" => "https://i.pinimg.com/564x/48/0c/a2/480ca2ea5ee29542ea14d7a2bceeedb6.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/mariane_card.jpg",
         "formação" => "Formada em medicina pela UFPA, com sua especialidade Ginecologia, tratando de questões fetais, cuidando diretamente da gestante e do feto, desde a sua geração até o seu nascimento."
       ],
 
       [
         "nome" => "Leandro Cardoso",
         "especialidade" => "Ginecologia",
-        "foto" => "https://i.pinimg.com/736x/95/ac/92/95ac92ab6997a27442d0e032bdb7c552.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/leandro_card.jpg",
         "formação" => "Formado em medicina pela UFAL, com sua especialidade Ginecologia, tratando de questões da mastologia, tratando das patologias que atingem as mamas, fazendo consultas de prevenção até retirada de nódulos mamários."
       ],
       
       [
         "nome" => "Lara Oliveira",
         "especialidade" => "Ginecologia",
-        "foto" => "https://i.pinimg.com/564x/e1/60/a8/e160a83399b412700f167b8a723ee56a.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/lara_card.jpg",
         "formação" => "Formada em medicina pela UFMT, com sua especialidade Ginecologia, especialista em oncologia pélvica,cuida dos tumores malignos que envolvem os órgãos sexuais femininos, como a vulva, vagina, colo de útero, endomério entre outros, seu método deixa esse tratamento mais leve e menos estressante."
       ],
 
       [
         "nome" => "Ana Luisa Barbosa",
         "especialidade" => "Ginecologia",
-        "foto" => "https://i.pinimg.com/564x/10/21/5b/10215b1d62cdbda48095507854e39e63.jpg",
+        "foto" => "../../Projeto-Web/img/medicos/analuisa_card.jpg",
         "formação" => "Formada em medicina pela UERJ, com sua especialidade Ginecologia, cuidando da Ginecologia Infantopuberal,para cuidar da sua filha desde cedo, fazendo tratamentos preventivos e atuando em possíveis alterações nos hormônios da criança ."
       ],
      /* ----------------------------Parte dos dentistas-------------------------------------------------------------------------------*/
@@ -149,28 +148,28 @@ $medicos =[
      [
       "nome" => "Nicole Costa",
       "especialidade" => "Odontologia",
-      "foto" => "https://i.pinimg.com/564x/35/5b/52/355b524b3c913230f06d36840976b3c9.jpg",
+      "foto" => "../../Projeto-Web/img/medicos/nicole_card.jpg",
       "formação" => "Formada em odontologia pela UFRJ, com sua especialidade Endodontia, tratando de enfermidades da polpa e da raiz do dente."
     ],
 
     [
       "nome" => "Murilo Araujo",
       "especialidade" => "Odontologia",
-      "foto" => "https://i.pinimg.com/564x/c1/f9/0a/c1f90acdb98132d9e63938e6c63ada7b.jpg",
+      "foto" => "../../Projeto-Web/img/medicos/muriloaraujo.jpg",
       "formação" => "Formado em Odontologia pela UFOB, com sua especialidade Periodontia, cuidando sua gengiva e tratar de problemas como gengivite, placas bacterianas, periodontite."
     ],
 
     [
       "nome" => "Victor Correia ",
       "especialidade" => "Odontologia",
-      "foto" => "https://i.pinimg.com/564x/27/44/2b/27442b1b978a94dac99a643ffad99438.jpg",
+      "foto" => "../../Projeto-Web/img/medicos/victor_card.jpg",
       "formação" => "Formado em odontologia pela UESB, com sua especialidade Ortodontia, sua especialidade permite tratar com cuidado a posição dos dentes, e dos ossos maxilares."
     ],
 
     [
       "nome" => "Evelyn Almeida",
       "especialidade" => "Odontologia",
-      "foto" => "https://i.pinimg.com/236x/06/d9/6e/06d96ed1ff732c70435cfcfb1c75e798.jpg",
+      "foto" => "../../Projeto-Web/img/medicos/evelyn_card.jpg",
       "formação" => "Formada em odontologia pela UFSC, com sua especialidade Implantodontia, cuida do setor de implantes de dentes da maneira mais tecnológica e acessível para você."
     ],
 
