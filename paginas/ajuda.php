33c2362dc9213afea25ed1dba28b678b6027f53b
iLevsPHP_ <202215210015@ifba.edu.br>
2023-05-26 00:07:11 -0300

    Adição do footer (rodapé)
    Finalmente finalizado esse roda pé

--- paginas/ajuda.php
+++ paginas/ajuda.php
@@ -40,7 +40,7 @@
             <img src="../img/logo-rodape.png" class="logo-rodape">
             <div class="wrapper">
 
-                <div id="sub-e">
+                <div id="sub-e" class="sub">
                     <h3>Especialidades</h3>
                     <p>Cardiologia</p>
                     <p>Cirurgia Plastica</p>
@@ -48,24 +48,41 @@
                     <p>Ginecologia</p>
                     <p>Odontologia</p>
                 </div>
-                <div id="sub-cidade">
-                    <h3>Estado</h3>
+                <div id="sub-cidade" class="sub">
+                    <h3>Estados</h3>
                     <p>Bahia</p>
                     <p>Minas Gerais</p>
                     <p>São Paulo</p>
                     <p>Pernambuco</p>
                     <p>Rio De Janeiro</p>
                 </div>
+                <div id="sub-empresa" class="sub">
+                    <h3>Empresa</h3>
+                    <p>Sobre nós</p>
+                    <p>Centra de Ajuda</p>
+                    <p>Status</p>
+                    <p>Localidades</p>
+                </div>
+            </div>
+            <div id="app">
+                <h4>Baixe o nosso App: O Consulta+ Plus</h4>
+                <a href="" title="App"><img src="../img/baixe.png"></a>
             </div>
             <div class="redes-sociais">
-                <a href="" title="Facebook"><img src="img/face.png"></a>
-                <a href="" title="Instagram"><img src="img/insta.png"></a>
-                <a href="" title="Whatsapp"><img src="img/whats.png"></a>
+                <a href="" title="Facebook"><img src="../img/face.png" class="redes"></a>
+                <a href="" title="Instagram"><img src="../img/insta.png" class="redes"></a>
+                <a href="" title="Whatsapp" ><img src="../img/whats.png" class="redes"></a>
             </div>
         </div>
         <div id="roda-2">
-            <p class="ui-minha-autoral">Todos os Direitos Reservados &copy; Consulta+ 2023</p>
+            <div id="r2-1" class="r2-p">
+                <p>Termos de Uso</p>
+                <p>Politicas de Privacidade</p>
+            </div>
+            <div id="r2-2" class="r2-p">
+                <p class="ui-minha-autoral">Todos os Direitos Reservados &copy; Consulta+ 2023</p>
+            </div>
         </div>
-    </footer>   
+    </footer>      
 </body>
 </html>
--- paginas/contato.php
+++ paginas/contato.php
@@ -83,8 +83,54 @@
                     <a href="index.html"><img src="imagens/logo/logo.png.png"></a>
                 </div>
                 </main>
-    <footer id="rodape">
-    
-    </footer>    
+                <footer id="rodape">
+        <div id="roda-1">
+            <img src="../img/logo-rodape.png" class="logo-rodape">
+            <div class="wrapper">
+
+                <div id="sub-e" class="sub">
+                    <h3>Especialidades</h3>
+                    <p>Cardiologia</p>
+                    <p>Cirurgia Plastica</p>
+                    <p>Nutrição</p>
+                    <p>Ginecologia</p>
+                    <p>Odontologia</p>
+                </div>
+                <div id="sub-cidade" class="sub">
+                    <h3>Estados</h3>
+                    <p>Bahia</p>
+                    <p>Minas Gerais</p>
+                    <p>São Paulo</p>
+                    <p>Pernambuco</p>
+                    <p>Rio De Janeiro</p>
+                </div>
+                <div id="sub-empresa" class="sub">
+                    <h3>Empresa</h3>
+                    <p>Sobre nós</p>
+                    <p>Centra de Ajuda</p>
+                    <p>Status</p>
+                    <p>Localidades</p>
+                </div>
+            </div>
+            <div id="app">
+                <h4>Baixe o nosso App: O Consulta+ Plus</h4>
+                <a href="" title="App"><img src="../img/baixe.png"></a>
+            </div>
+            <div class="redes-sociais">
+                <a href="" title="Facebook"><img src="../img/face.png" class="redes"></a>
+                <a href="" title="Instagram"><img src="../img/insta.png" class="redes"></a>
+                <a href="" title="Whatsapp" ><img src="../img/whats.png" class="redes"></a>
+            </div>
+        </div>
+        <div id="roda-2">
+            <div id="r2-1" class="r2-p">
+                <p>Termos de Uso</p>
+                <p>Politicas de Privacidade</p>
+            </div>
+            <div id="r2-2" class="r2-p">
+                <p class="ui-minha-autoral">Todos os Direitos Reservados &copy; Consulta+ 2023</p>
+            </div>
+        </div>
+    </footer>      
     </body>
     </html>
